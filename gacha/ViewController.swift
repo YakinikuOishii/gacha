2159fe3dff44a60e3b1b77af7eeb06be5b2eab36
Miku <fum_o3o7@ezweb.ne.jp>
2019-09-09 23:19:11 +0900

    Initial Commit

---
+++ gacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gacha
+//
+//  Created by 笠原未来 on 2019/09/09.
+//  Copyright © 2019 笠原未来. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
